018852069811c6aae1c02e99a4e9feecf9add2cd
cmb-sy <free.nakashima@gmail.com>
2024-04-10 11:04:45 +0900

    update

--- src/componemt/Navigation/Navigation.tsx
+++ src/componemt/Navigation/Navigation.tsx
@@ -2,23 +2,25 @@ import React from "react";
 import "../Navigation/Navigation.css";
 import { Link } from "react-router-dom";
 
-type Item = {
-  id: number;
+type HeaderItem = {
   value: string;
   url: string;
 };
 
 function Navigation() {
-  const items: Item[] = [
-    { id: 1, value: "ホーム", url: "/" },
-    { id: 2, value: "経歴", url: "/background" },
+  const headerItems: HeaderItem[] = [
+    { value: "ホーム", url: "/" },
+    { value: "経歴", url: "/background" },
+    { value: "作品", url: "/works" },
+    { value: "技術や資格", url: "/skill" },
+    { value: "お問合せ", url: "/contact" },
   ];
 
   return (
     <nav>
       <ul className="nav-links">
-        {items.map((item) => (
-          <li key={item.id}>
+        {headerItems.map((item) => (
+          <li>
             <Link to={item.url}>{item.value}</Link>
           </li>
         ))}
