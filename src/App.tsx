f1e6c9ce3b40c5b6a1e36c654f54279e2a38db4b
cmb-sy <free.nakashima@gmail.com>
2024-04-16 18:21:43 +0900

    bootstrapの導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import React from "react";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
 import Header from "./componemt/Header/HeaderSection";
 import Career from "./componemt/Career/CareerSection";
 import HomePage from "./componemt/Home/HomePage";
 import About from "./componemt/AboutMe/AboutSection";
 import Works from "./componemt/Works/WorksSection";
 import Skill from "./componemt/Skill/SkillSection";
-import Menu from "./componemt/Test/Menu";
+import BasicExample from "./componemt/Test/Menu";
+
 function App() {
   return (
     <div className="App">
@@ -15,10 +17,7 @@ function App() {
       <About />
       <Career />
       <Works />
-      <Menu title="親メニュー" withIcon={true}>
-        <Menu title="子メニュー1" />
-        <Menu title="子メニュー2" />
-      </Menu>
+      <BasicExample />
       <Skill />
     </div>
   );
--- src/componemt/Test/Menu.tsx
+++ src/componemt/Test/Menu.tsx
@@ -1,52 +1,47 @@
-import { FC, ReactNode, useEffect, useRef, useState } from "react";
-import Chevron from "../../assets/chevron.svg";
-import styles from "./Menu.module.css";
+import Accordion from "react-bootstrap/Accordion";
 
-type Props = {
-  title: string;
-  withIcon?: boolean;
-  children?: ReactNode;
-};
-
-function Menu({ title, withIcon, children }: Props) {
-  const childElement = useRef<HTMLDivElement>(null);
-  const [showChildren, setShowChildren] = useState(false);
-  const [childHeight, setChildHeight] = useState(0);
-  const [reverseIcon, setReverseIcon] = useState(false);
-
-  useEffect(() => {
-    if (childElement.current) {
-      const height = childElement.current.clientHeight;
-      setChildHeight(height);
-    }
-  }, [childHeight]);
-
-  const handleClick = () => {
-    setShowChildren(!showChildren);
-    setReverseIcon(!reverseIcon);
-  };
-
+function AllCollapseExample() {
   return (
-    <>
-      <div onClick={handleClick} className={styles.item}>
-        {title}
-        {withIcon && (
-          <div className={`${styles.icon} ${reverseIcon && styles.reverse}`}>
-            aaaa
-          </div>
-        )}
-      </div>
-      <div
-        className={styles.childItem}
-        style={{
-          height: children && showChildren ? `${childHeight}px` : "0px",
-          opacity: children && showChildren ? 1 : 0,
-        }}
-      >
-        <div ref={childElement}>{children}</div>
-      </div>
-    </>
+    <Accordion>
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>Accordion Item #12345</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum
+          dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+          commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+          velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+          occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+          mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur
+          adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+          irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+          fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+          sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="1">
+        <Accordion.Header>Accordion Item #2</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
   );
 }
 
-export default Menu;
+export default AllCollapseExample;
