302495e9121100362bb7562380b91317a6f47152
cmb-sy <free.nakashima@gmail.com>
2024-04-17 10:57:55 +0900

    update

--- src/componemt/Works/WorksSection.tsx
+++ src/componemt/Works/WorksSection.tsx
@@ -7,12 +7,35 @@ function worksSection() {
       <h1>Works</h1>
       <div className="worksArea">
         {WorkLists.map((work) => (
-          <div className="serviceBox">
-            <h3>{work.title}</h3>
-            <p>{work.description}</p>
+          <div className="l-wrapper_06">
+            <div className="card_06">
+              <img
+                className="card-img_06"
+                src="https://dubdesign.net/wp-content/uploads/2020/05/0508_dtplayouteyecatch.jpg"
+                alt=""
+              />
+              <div className="card-content_06">
+                <p className="card-title_06">{work.title}</p>
+                <p className="card-text_06">{work.description}</p>
+              </div>
+              <div className="card-link_06">
+                <a href="#">HOME</a>
+              </div>
+            </div>
           </div>
         ))}
       </div>
+
+      {/* <div classNameName="worksArea">
+        {WorkLists.map((work) => (
+          <div classNameName="serviceBox">
+            <img src={work.imgUrl} />
+            <h3>{work.title}</h3>
+            <p>{work.description}</p>
+            <button>sssss</button>
+          </div>
+        ))}
+      </div> */}
     </section>
   );
 }
--- src/componemt/Works/worksList.ts
+++ src/componemt/Works/worksList.ts
@@ -1,14 +1,30 @@
 export type WorkList = {
   title: string;
   description: string;
-  url: string;
+  imgUrl?: string;
+  sourceUrl?: string;
 };
 
 export const WorkLists: WorkList[] = [
-  { title: "テスト", description: "説明文", url: "http" },
-  { title: "テスト", description: "説明文", url: "http" },
-  { title: "テスト", description: "説明文", url: "http" },
-  { title: "テスト", description: "説明文", url: "http" },
-  { title: "テスト", description: "説明文", url: "http" },
-  { title: "テスト", description: "説明文", url: "http" },
+  {
+    title: "テスト",
+    description: "説明文",
+    imgUrl: "https://avatars.githubusercontent.com/u/63276819?v=4",
+    sourceUrl: "https://avatars.githubusercontent.com/u/63276819?v=4",
+  },
+  {
+    title: "テスト",
+    description: "説明文",
+    imgUrl: "https://avatars.githubusercontent.com/u/63276819?v=4",
+  },
+  {
+    title: "テスト",
+    description: "説明文",
+    imgUrl: "https://avatars.githubusercontent.com/u/63276819?v=4",
+  },
+  {
+    title: "テスト",
+    description: "説明文",
+    imgUrl: "https://avatars.githubusercontent.com/u/63276819?v=4",
+  },
 ];
