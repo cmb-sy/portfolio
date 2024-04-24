64835c7bd135c6dd5f70907086bc3ba15701b668
cmb-sy <free.nakashima@gmail.com>
2024-04-24 10:56:55 +0900

    ハンバーガーメニューの作成

--- src/componemt/Header/HeaderSection.tsx
+++ src/componemt/Header/HeaderSection.tsx
@@ -2,25 +2,35 @@ import { Link } from "react-router-dom";
 import "./Header.css";
 import { navigationItems } from "./HeaderItem";
 
+// const burger: Element | null = document.querySelector(".burger");
+// const nav = document.querySelector(".nav");
+
+// burger.addEventListener("click", () => {
+//   nav?.classList.toggle("nav-active");
+// });
+
 export const Header = () => {
   return (
-    <nav>
-      <div className="logo">
-        <h4>ナビゲーションバー</h4>
-      </div>
-      <ul className="nav-links">
-        {navigationItems.map((item) => (
-          <li>
-            <Link to={item.url}>{item.value}</Link>
-          </li>
-        ))}
-      </ul>
-      <div className="burger">
-        <div className="line1"></div>
-        <div className="line2"></div>
-        <div className="line3"></div>
-      </div>
-    </nav>
+    <body className="nav-body">
+      <nav>
+        <div className="logo">
+          <h4>ナビゲーションバー</h4>
+        </div>
+        <ul className="nav-links">
+          {navigationItems.map((item) => (
+            <li>
+              <Link to={item.url}>{item.value}</Link>
+            </li>
+          ))}
+        </ul>
+        {/* ハンバーガメニュー */}
+        <div className="burger">
+          <div className="line1"></div>
+          <div className="line2"></div>
+          <div className="line3"></div>
+        </div>
+      </nav>
+    </body>
   );
 };
 
