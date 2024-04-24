e302280f2577d31e763d356cfe69b291bbafe709
cmb-sy <free.nakashima@gmail.com>
2024-04-24 13:41:18 +0900

    開閉を制御

--- src/componemt/Header/HeaderSection.tsx
+++ src/componemt/Header/HeaderSection.tsx
@@ -1,35 +1,38 @@
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Header.css";
 import { navigationItems } from "./HeaderItem";
 
-// const burger: Element | null = document.querySelector(".burger");
-// const nav = document.querySelector(".nav");
-
-// burger.addEventListener("click", () => {
-//   nav?.classList.toggle("nav-active");
-// });
-
 export const Header = () => {
+  const [navActive, setNavActive] = useState(false);
+
+  const toggleNav = () => {
+    setNavActive(!navActive);
+  };
+
   return (
-    <body className="nav-body">
+    <body>
       <nav>
         <div className="logo">
           <h4>ナビゲーションバー</h4>
         </div>
-        <ul className="nav-links">
+        <ul className={navActive ? "nav-links nav-active" : "nav-links"}>
           {navigationItems.map((item) => (
-            <li>
-              <Link to={item.url}>{item.value}</Link>
+            <li key={item.url}>
+              <Link to={item.url} onClick={toggleNav}>
+                {item.value}
+              </Link>
             </li>
           ))}
         </ul>
         {/* ハンバーガメニュー */}
-        <div className="burger">
+        <div className="burger" onClick={toggleNav}>
           <div className="line1"></div>
           <div className="line2"></div>
           <div className="line3"></div>
         </div>
       </nav>
+      <script src="script.js"></script>
     </body>
   );
 };
