a73ca8d612cb8c326159e1df63a85b20537ac29c
cmb-sy <free.nakashima@gmail.com>
2024-05-04 22:11:03 +0900

    update

--- src/componemt/Works/WorksSection.tsx
+++ src/componemt/Works/WorksSection.tsx
@@ -2,30 +2,26 @@ import styles from "./Works.module.css";
 import Heading from "../Heading/Headeing";
 import { WorkLists } from "./worksList";
 
-function worksSection() {
+function WorksSection() {
+  // 関数名をPascalCaseに変更
   return (
     <section id="work">
       <div className={styles.workArea}>
         <Heading headingTitle="Work" description="過去に作成した作品です。" />
         <div className={styles.worksArea}>
           {WorkLists.map((work) => (
-            <a href={work.sourceUrl} className={styles.yyy}>
+            <a href={work.sourceUrl} className={styles.workLink}>
               <div className={styles.card}>
-                <img
-                  className={styles.cardimg_06}
-                  src={work.imgUrl}
-                  alt=""
-                  // style={{ width: "20rem", height: "10rem" }}
-                />
-                <div className={styles.cardlink_06}>
-                  <div className={styles.testkk}>
+                <img className={styles.cardImg} src={work.imgUrl} alt="work" />
+                <div className={styles.cardLink}>
+                  <div className={styles.tagContainer}>
                     {work.tag.map((item) => (
                       <span className={styles.tag}>{item}</span>
                     ))}
                   </div>
-                  <div className={styles.cardcontent_06}>
-                    <p className={styles.cardtitle_06}>{work.title}</p>
-                    <span className={styles.carddescription}>
+                  <div className={styles.cardContent}>
+                    <p className={styles.cardTitle}>{work.title}</p>
+                    <span className={styles.cardDescription}>
                       {work.description}
                     </span>
                   </div>
@@ -39,4 +35,4 @@ function worksSection() {
   );
 }
 
-export default worksSection;
+export default WorksSection; // 関数名をPascalCaseに変更
