57d156fee96832dfc52e02c642fbe341203ef48c
cmb-sy <free.nakashima@gmail.com>
2024-10-26 19:21:08 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import "./App.css";
 
 import Header from "./componemt/Header/HeaderSection";
-import Home from "./componemt/Home/HomeSection";
 import About from "./componemt/AboutMe/AboutSection";
 import Career from "./componemt/Career/CareerSection";
 import Work from "./componemt/Works/WorksSection";
@@ -12,7 +11,6 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Home />
       <About />
       <Career />
       <Work />
