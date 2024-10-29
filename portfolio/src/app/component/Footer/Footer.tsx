07eeb10d9c678af19d76af62c9b26ae0148500e7
cmb-sy <free.nakashima@gmail.com>
2024-10-29 16:08:22 +0900

    update

--- portfolio/src/app/component/Footer/Footer.tsx
+++ portfolio/src/app/component/Footer/Footer.tsx
@@ -12,17 +12,19 @@ const Footer: React.FC = () => {
 
   return (
     <footer className="text-black w-full z-10 relative flex flex-col items-center">
-      <div className="bg-gray-600 w-full py-32 flex justify-center relative">
-        <div className="text-xl text-white">Thank You for visiting </div>
+      <div className="bg-gray-600 w-full py-16 md:py-32 flex justify-center relative">
+        <div className="text-base md:text-xl text-white">
+          Thank You for visiting{" "}
+        </div>
         <button
-          className="absolute bottom-0 transform translate-y-2/3 bg-gray-800 text-white rounded-full w-36 h-36 flex items-center justify-center hover:translate-y-1/2 transition-transform duration-300"
+          className="absolute bottom-0 transform translate-y-2/3 bg-gray-800 text-white rounded-full w-24 h-24 md:w-36 md:h-36 flex items-center justify-center hover:translate-y-1/2 transition-transform duration-300"
           onClick={scrollToAboutMe}
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 48 48"
-            className="w-24 h-24 text-white"
+            className="w-16 h-16 md:w-24 md:h-24 text-white"
             style={{ transform: "rotate(180deg)" }}
           >
             <path
@@ -32,10 +34,10 @@ const Footer: React.FC = () => {
           </svg>
         </button>
       </div>
-      <div className="bg-gray-800 w-full flex flex-col items-center pt-12">
-        <div className="container mx-auto flex justify-between items-center py-4">
-          <div className="text-sm text-white">
-            &copy; 2023 Your Company. All rights reserved.
+      <div className="bg-gray-800 w-full flex flex-col items-center pt-6 md:pt-12">
+        <div className="container mx-auto flex flex-col md:flex-row justify-between items-center py-4">
+          <div className="text-xs md:text-sm text-white self-start md:self-auto ml-5">
+            &copy; All rights reserved.
           </div>
         </div>
       </div>
